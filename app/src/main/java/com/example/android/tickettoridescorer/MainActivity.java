271183bd72245f3bcc530339e60aac1bbc06c364
jae0120 <Jasmineeyre007@gmail.com>
2018-04-06 20:48:53 -0500

    Added background and 5-player capability.

--- app/src/main/java/com/example/android/tickettoridescorer/MainActivity.java
+++ app/src/main/java/com/example/android/tickettoridescorer/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
     int redPlayerScore = 1;
     int greenPlayerScore = 1;
+    int bluePlayerScore = 1;
+    int yellowPlayerScore = 1;
+    int blackPlayerScore = 1;
 
     // display Red's Score
     private void displayRedScore(int redScore) {
@@ -26,6 +29,24 @@ public class MainActivity extends AppCompatActivity {
     private void displayGreenScore(int greenScore) {
         TextView greenScoreText = (TextView) findViewById(R.id.green_score_text_view);
         greenScoreText.setText("" + greenScore);
+    }
+
+    // display Blue's Score
+    private void displayBlueScore(int blueScore) {
+        TextView blueScoreText = (TextView) findViewById(R.id.blue_score_text_view);
+        blueScoreText.setText("" + blueScore);
+    }
+
+    // display Yellow's Score
+    private void displayYellowScore(int yellowScore) {
+        TextView yellowScoreText = (TextView) findViewById(R.id.yellow_score_text_view);
+        yellowScoreText.setText("" + yellowScore);
+    }
+
+    // display Black's Score
+    private void displayBlackScore(int blackScore) {
+        TextView blackScoreText = (TextView) findViewById(R.id.black_score_text_view);
+        blackScoreText.setText("" + blackScore);
     }
 
     // add one to Red's Score for one track
@@ -100,11 +121,126 @@ public class MainActivity extends AppCompatActivity {
         displayGreenScore(greenPlayerScore);
     }
 
+    // add one to Blue's Score for one track
+    public void oneTrackBlue(View view) {
+        bluePlayerScore++;
+        displayBlueScore(bluePlayerScore);
+    }
+
+    // add two to Blue's Score for two tracks
+    public void twoTracksBlue(View view) {
+        bluePlayerScore += 2;
+        displayBlueScore(bluePlayerScore);
+    }
+
+    // add four to Blue's Score for three tracks
+    public void threeTracksBlue(View view) {
+        bluePlayerScore += 4;
+        displayBlueScore(bluePlayerScore);
+    }
+
+    // add 7 to Blue's Score for 4 tracks
+    public void fourTracksBlue(View view) {
+        bluePlayerScore += 7;
+        displayBlueScore(bluePlayerScore);
+    }
+
+    // add 10 to Blue's Score for 5 tracks
+    public void fiveTracksBlue(View view) {
+        bluePlayerScore += 10;
+        displayBlueScore(bluePlayerScore);
+    }
+
+    // add 15 to Blue's Score for 6 tracks
+    public void sixTracksBlue(View view) {
+        bluePlayerScore += 15;
+        displayBlueScore(bluePlayerScore);
+    }
+
+
+    // add one to Yellow's Score for one track
+    public void oneTrackYellow(View view) {
+        yellowPlayerScore++;
+        displayYellowScore(yellowPlayerScore);
+    }
+
+    // add two to Yellow's Score for two tracks
+    public void twoTracksYellow(View view) {
+        yellowPlayerScore += 2;
+        displayYellowScore(yellowPlayerScore);
+    }
+
+    // add four to Yellow's Score for three tracks
+    public void threeTracksYellow(View view) {
+        yellowPlayerScore += 4;
+        displayYellowScore(yellowPlayerScore);
+    }
+
+    // add 7 to Yellow's Score for 4 tracks
+    public void fourTracksYellow(View view) {
+        yellowPlayerScore += 7;
+        displayYellowScore(yellowPlayerScore);
+    }
+
+    // add 10 to Yellow's Score for 5 tracks
+    public void fiveTracksYellow(View view) {
+        yellowPlayerScore += 10;
+        displayYellowScore(yellowPlayerScore);
+    }
+
+    // add 15 to Yellow's Score for 6 tracks
+    public void sixTracksYellow(View view) {
+        yellowPlayerScore += 15;
+        displayYellowScore(yellowPlayerScore);
+    }
+
+    // add one to Black's Score for one track
+    public void oneTrackBlack(View view) {
+        blackPlayerScore++;
+        displayBlackScore(blackPlayerScore);
+    }
+
+    // add two to Black's Score for two tracks
+    public void twoTracksBlack(View view) {
+        blackPlayerScore += 2;
+        displayBlackScore(blackPlayerScore);
+    }
+
+    // add four to Black's Score for three tracks
+    public void threeTracksBlack(View view) {
+        blackPlayerScore += 4;
+        displayBlackScore(blackPlayerScore);
+    }
+
+    // add 7 to Black's Score for 4 tracks
+    public void fourTracksBlack(View view) {
+        blackPlayerScore += 7;
+        displayBlackScore(blackPlayerScore);
+    }
+
+    // add 10 to Black's Score for 5 tracks
+    public void fiveTracksBlack(View view) {
+        blackPlayerScore += 10;
+        displayBlackScore(blackPlayerScore);
+    }
+
+    // add 15 to Black's Score for 6 tracks
+    public void sixTracksBlack(View view) {
+        blackPlayerScore += 15;
+        displayBlackScore(blackPlayerScore);
+    }
+
     //resets both score
     public void resetScores(View view) {
         redPlayerScore = 1;
         greenPlayerScore = 1;
+        bluePlayerScore = 1;
+        yellowPlayerScore = 1;
+        blackPlayerScore = 1;
+        displayBlueScore(bluePlayerScore);
         displayGreenScore(greenPlayerScore);
         displayRedScore(redPlayerScore);
+        displayYellowScore(yellowPlayerScore);
+        displayBlackScore(blackPlayerScore);
     }
 }
